a397bb3a71e8e07a6dd70a85815c685d4020a9fa
David Jones <you@example.com>
2018-02-03 19:41:14 +0000

    Reinstate samtools

--- build/opt-build.sh
+++ build/opt-build.sh
@@ -11,6 +11,7 @@ set -u
 VER_HTSLIB="1.7"
 VER_LIBBW="0.4.2"
 VER_CGPBIGWIG="0.5.0-rc1"
+VER_SAMTOOLS="1.7"
 
 
 if [ "$#" -lt "1" ] ; then
@@ -71,6 +72,21 @@ if [ ! -e $SETUP_DIR/htslib.success ]; then
   cd $SETUP_DIR
   rm -rf distro.*
   touch $SETUP_DIR/htslib.success
+fi
+
+## SAMTOOLS (tar.bz2)
+if [ ! -e $SETUP_DIR/samtools.success ]; then
+  curl -sSL --retry 10 https://github.com/samtools/samtools/releases/download/${VER_SAMTOOLS}/samtools-${VER_SAMTOOLS}.tar.bz2 > distro.tar.bz2
+  rm -rf distro/*
+  tar --strip-components 1 -C distro -xjf distro.tar.bz2
+  cd distro
+  ./configure --enable-plugins --enable-libcurl --with-htslib=$INST_PATH --prefix=$INST_PATH
+  make clean
+  make -j$CPU all
+  make install
+  cd $SETUP_DIR
+  rm -rf distro.* distro/*
+  touch $SETUP_DIR/samtools.success
 fi
 
 ## LIB-BW (tar.gz)
